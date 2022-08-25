f41a64a717a365e83e2f95eaf04fa64bd0f9ae4a
julianmolina99 <juliandavidmv@ufps.edu.co>
2022-08-25 11:08:41 -0500

    [ADDED] components, common, dashboard

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,9 @@
 const colors = require('tailwindcss/colors');
 
 module.exports = {
-  content: ['./src/pages/**/*.{html,jsx,js}'],
+  content: ['./src/**/*.{html,jsx,js}'],
+
+
   theme: {
     colors: {
       ...colors,
