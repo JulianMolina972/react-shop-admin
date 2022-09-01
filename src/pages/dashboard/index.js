a572f65f56028abd01f1ea7bedeafdaeb971625b
julianmolina99 <juliandavidmv@ufps.edu.co>
2022-09-01 13:22:06 -0500

    [ADDED] Chart

--- src/pages/dashboard/index.js
+++ src/pages/dashboard/index.js
@@ -1,16 +1,6 @@
-const people = [
-  {
-    name: 'Jane Cooper',
-    title: 'Regional Paradigm Technician',
-    department: 'Optimization',
-    role: 'Admin',
-    email: 'jane.cooper@example.com',
-    image: 'https://images.unsplash.com/photo-1494790108377-be9c29b29330?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=4&w=256&h=256&q=60',
-  },
-];
-
 import endPoints from "@services/api";
 import useFetch from "@hooks/useFetch";
+
 
 const PRODUCT_LIMIT = 10;
 const PRODUCT_OFFSET = 0;
