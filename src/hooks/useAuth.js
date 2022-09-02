31ff5055f4a40863d6cc4a3d59a2353d8b606fb7
julianmolina99 <juliandavidmv@ufps.edu.co>
2022-09-02 17:51:02 -0500

    Logout implementation

--- src/hooks/useAuth.js
+++ src/hooks/useAuth.js
@@ -45,10 +45,18 @@ function useProvideAuth() {
     }
   }
 
+  const logout = () => {
+    Cookie.remove('token');
+    setUser(null);
+    delete axios.defaults.headers.Authorization;
+    window.location.href = '/login';
+  }
+
   return {
     user,
     signIn,
     error,
     setError,
+    logout,
   }
 }
